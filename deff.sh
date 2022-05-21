9efcc5f6e2b1df1f8b4e1e6782ec6166704f5eae
Evgen <nvvow.99@mail.ru>
2022-05-21 22:26:51 +0300

    dz

---
+++ deff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd hello
+mkdir number1 number2 number3
+cd number2
+touch there.txt here.txt where.txt one.json two.json
+mkdir number4 number5 number6
+ls -la 
+mv there.txt here.txt number5 
